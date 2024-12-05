41f2fc2fbd0e95b99499417264a5c6d2d96c7471
Michele Buccoli <m.buccoli@gmail.com>
2024-12-05 18:48:53 +0100

    Final version

--- src/day5.py
+++ src/day5.py
@@ -3,6 +3,7 @@
 from typing import Union
 from pathlib import Path
 from common import INPUT_DIR, check_test
+from graphlib import TopologicalSorter
 import numpy as np
 
 # %%
@@ -14,6 +15,7 @@ class Day5Quiz:
         self.quiz_fn = quiz_fn
         self.rules = None
         self.updates = None
+        self.graph={}
         self.correct_updates = None
 
     def parse(self, text):
@@ -29,17 +31,22 @@ class Day5Quiz:
         
     def refine_data(self, rules, updates):
         rules_hash = {}
+        self.graph={} # this shouldn't be an object, but let's make it so
         for vl, vr in rules:
             rule_l = rules_hash.get(vl, {"before":[], "after":[]})
             rule_l["after"].append(vr) # all numbers that must be after the number vl
             rule_r = rules_hash.get(vr, {"before":[], "after":[]})
             rule_r["before"].append(vl) # all numbers that must be before the number vr
             rules_hash.update({vl:rule_l, vr:rule_r})
+            edges = self.graph.get(vl, set())
+            edges.add(vr)
+            self.graph[vl]=edges
             
         return rules_hash, updates
     def filter_updates(self, rules, updates):
         
         filtered_updates=[]
+        wrong_updates=[]
         for update in updates:
             rule_broken=False
             for i, num_i in enumerate(update[:-1]):
@@ -52,10 +59,22 @@ class Day5Quiz:
                 if rule_broken:
                     break
             if rule_broken:
+                wrong_updates.append(update)
                 continue
             filtered_updates.append(update)
                 
-        return filtered_updates
+        return filtered_updates, wrong_updates
+    
+    def fix_updates(self, wrong_updates):
+        fixed_updates=[]
+        for update in wrong_updates:
+            subgraph={v:self.graph.get(v, set()).intersection(set(update)) for v in update}
+            ts= TopologicalSorter(subgraph)
+            fixed_updates.append(list(ts.static_order()))
+
+           
+        return fixed_updates    
+
     def sum_middle_number(self, updates):
         num=0
         for update in updates:
@@ -75,12 +94,16 @@ class Day5Quiz:
     def solve_quiz1(self, test_data=None):
         rules, updates = self.get_data(test_data)
         rules, updates = self.refine_data(rules, updates)
-        updates = self.filter_updates(rules, updates)
+        updates, _ = self.filter_updates(rules, updates)
 
         return self.sum_middle_number(updates)
 
     def solve_quiz2(self, test_data=None):
-        pass
+        rules, updates = self.get_data(test_data)
+        rules, updates = self.refine_data(rules, updates)
+        _, wrong_updates = self.filter_updates(rules, updates)
+        fixed_updates = self.fix_updates(rules, wrong_updates)
+        return self.sum_middle_number(fixed_updates)
 
 
 if __name__ == "__main__":
@@ -120,8 +143,8 @@ if __name__ == "__main__":
     result_test1=d5q.solve_quiz1(test_data=test_data)
     check_test(1, result_test1, true_result=143)
     print("Quiz1 result is", d5q.solve_quiz1())
-    # result_test2=d5q.solve_quiz2(test_data=test_data) 
-    # check_test(2, result_test2, true_result=9)    
-    # print("Quiz2 result is", d5q.solve_quiz2())
+    result_test2=d5q.solve_quiz2(test_data=test_data) 
+    check_test(2, result_test2, true_result=123)    
+    print("Quiz2 result is", d5q.solve_quiz2())
 
 # %%
