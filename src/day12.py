a622cdd8a91e60d4257d147f4c1b709e5ec432d0
Michele Buccoli <m.buccoli@gmail.com>
2024-12-12 21:12:22 +0100

    first quiz solved

--- src/day12.py
+++ src/day12.py
@@ -10,20 +10,28 @@ class Region:
         self.plant=plant
         self.idxs=[]
         self.edges=0
+        self.discounted_edges=0
+        self.shape = shape
         self.to_visit=np.zeros(shape)
         self.visited=np.zeros(shape)
     def add_idx(self, idx):
-        if self.visited[*idx]==0: # if it is already visited, we skip
-            self.idxs.append(idx)
-            self.to_visit[*idx]=1
+        if self.visited[*idx]==1: # if it is already visited, we skip
+            return
+        if self.to_visit[*idx]==1: # if it is already marked as to be visited
+            return
+        self.idxs.append(idx)
+        self.to_visit[*idx]=1
     def visit(self, idx):
-        self.to_visit[*idx]=0
         self.visited[*idx]=1
     def add_edge(self):
         self.edges+=1
+
+
     def compute_cost(self):
         return self.edges*len(self.idxs)
-
+    
+             
+        return self.edges*len(self.idxs)
 class Map:
     def __init__(self, map_):
         self.map=map_
@@ -33,26 +41,22 @@ class Map:
     def in_region(self, idx, region):
         return self.map[*idx]==region.plant
     
-    def get_first(self, where):
+    def get_first0(self, where):
         if np.all(where==1):
             return None
         idx0=np.where(where==0)
         idx = np.array([idx_i[0] for idx_i in idx0])
         return idx
     def get_first_region(self):
-        return self.get_first(self.visited)
-        # if np.all(self.visited==1):
-        #     return None
-        # idx0=np.where(self.visited==0)
-        # idx = np.array([idx_i[0] for idx_i in idx0])
-        # return idx
+        return self.get_first0(self.visited)
+
     def get_next_explore(self, region):
-        return self.get_first(1-region.to_visit) # strange but works
-        # if np.all(region.to_visit==0):
-        #     return None
-        # idx0=np.where(region.to_visit==0)
-        # idx = np.array([idx_i[0] for idx_i in idx0])
-        # return idx
+        # I want to get the first elem such that:
+        # 0 when to visit is 1 AND visited is 0
+        # (1-region_to_visit) + visited
+        
+        return self.get_first0((1-region.to_visit)+region.visited) # strange but works
+        
     def explore(self, region, idx):        
         self.visited[*idx]=1
         region.visit(idx)
@@ -65,7 +69,7 @@ class Map:
             if not self.in_region(idx_dest, region):
                 region.add_edge()
                 continue
-            region.add_idx(idx)
+            region.add_idx(idx_dest)
 
         
     def find_regions(self):
@@ -78,11 +82,17 @@ class Map:
                 self.explore(region, idx)
                 idx = self.get_next_explore(region)
 
-        idx = self.get_first_region()
+            idx = self.get_first_region()
+        return self.regions
     def compute_cost(self):
         cost=0
         for region in self.regions:
             cost+=region.compute_cost()
+        return cost
+    def compute_cost_discount(self):
+        cost=0
+        for region in self.regions:
+            cost+=region.compute_cost_discount()
         return cost
 
 
@@ -94,16 +104,23 @@ def solve_quiz1(fn=None, test_data=None):
     cost = map_.compute_cost()
     return cost, regions
 
+def solve_quiz2(fn=None, test_data=None):
+    data = get_data(fn, test_data)
+    map_ = parse_mat(data)
+    map_ = Map(map_)
+    regions = map_.find_regions()
+    cost = map_.compute_cost_discount()
+    return cost, regions
 
 if __name__=="__main__":
     quiz_fn = INPUT_DIR / "day12.txt"
 
 
     # TEST 0
-    test_data="""AAA000
-AAA000
-BBB111
-CCC111"""
+    test_data="""AA00
+AA00
+BB11
+CC11"""
     cost, regions = solve_quiz1(test_data=test_data)
 
     # TEST1
@@ -140,5 +157,5 @@ MMMISSJEEE"""
     check_test(1.3, cost, true_result=1930)
 
 
-    print("Quiz1 result is", solve_quiz1(fn=quiz_fn))
+    print("Quiz1 result is", solve_quiz1(fn=quiz_fn)[0])
 #    print("Quiz2 result is", solve_quiz1(fn=quiz_fn, blink_times=75))
