656dd16d8c19d986d38ceef2df445ff4a4c9501c
Michele Buccoli <m.buccoli@gmail.com>
2024-12-24 17:20:14 +0100

    quiz2 finally

--- src/day24.py
+++ src/day24.py
@@ -7,7 +7,7 @@ from common import INPUT_DIR, get_data, check_test,check_solution
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 def printd(*args):
-    print(*args)
+    #print(*args)
     pass
 
 # %%
@@ -29,26 +29,122 @@ def parse(text_data):
         op=op.lower()
         if op=="xor":
             op="!="
-        out_op = (output, var1, op, var2)
+        if var1 > var2: # let's keep input var with alphabetical order
+            var1, var2 = var2, var1
+        out_op = [output, var1, op, var2]
         out_ops.append(out_op)
         if output.startswith("z"):
             maxZ=max(maxZ, int(output[1:]))
             lastZ = l
     return {"input_dict":input_dict, "out_ops":out_ops, "maxZ":maxZ, "lastZ":lastZ}    
 
+def build_data2(data):
+    # WHAT DO I NEED?
+    # I need, for each var, to understand WHERE they were used.
+    Nbits=0
+    for x in data["input_dict"]:
+        Nbits=max(Nbits, int(x[1:]))
+    data["Nbits"]=Nbits+1 # number of bits actually used
+    cmd_as_in={}
+    cmd_as_out={}
+    def add_in_list(dict_, key_, val):
+        list_=dict_.get(key_, [])
+        list_.append(val)
+        dict_[key_]=list_
+        return dict_
+    for i, op_i in enumerate(data["out_ops"]):
+        o, v1, _, v2 = op_i
+        cmd_as_out = add_in_list(cmd_as_out, o, i)
+        cmd_as_in = add_in_list(cmd_as_in, v1, i)
+        cmd_as_in = add_in_list(cmd_as_in, v2, i)
+    data["cmd_as_in"]=cmd_as_in 
+    data["cmd_as_out"]=cmd_as_out
+    return data
+
 def build_data(data):
     out_ops =data["out_ops"]
     out_eval = []
     inp = lambda var : f"input_dict['{var}']"
         
- 
     for op_tuple in out_ops:
         _, var1, op, var2 = op_tuple
         cmd = f"{inp(var1)} {op} {inp(var2)}"
         out_eval.append(cmd)
     data["out_eval"] = out_eval
-    return data
+    return build_data2(data)
 
+input_var = lambda v, i: f"{v}{str(i).zfill(2)}"
+
+def find_out(data, var1=None, var2=None, op_="and"):
+    common_op = set(data["cmd_as_in"][var1]) & set(data["cmd_as_in"][var2])
+    for c in common_op:
+        out_name, v1, o, v2 = data["out_ops"][c]
+        if [v1, v2] in [[var1,var2], [var2, var1]] and o==op_:
+            return out_name, c
+    return "", -1
+                         
+def switch(data, out1, out2):
+    o1= data["cmd_as_out"][out1][0]
+    o2= data["cmd_as_out"][out2][0]
+    data["out_ops"][o1][0] = out2
+    data["out_ops"][o2][0] = out1
+    return build_data2(data) # rebuild dicts
+
+def find_ins(data, out):
+    c= data["cmd_as_out"][out][0]
+    op_line = data["out_ops"][c]
+    return op_line[1], op_line[3], c 
+def check_connection(data):
+    # now, hopefully the wires should be something like:
+    # z0 = x0 xor y0
+    # carry_0 = x0 and y0
+    #
+    #
+    # add_i = xi xor yi
+    # zi = add_i xor carry_{i-1}
+    #
+    # and_i = xi and yi
+    # carry_i_a = add_i AND carry_i-1
+    # carry_i  = carry_i_a OR and_i
+    # for now, let's just check
+    # 
+    assert ["z00", "x00", "!=", "y00"] in data["out_ops"], "This is the first problem"
+    carry0_var, _ = find_out(data, "x00", "y00", "and") # find report    
+    
+    assert carry0_var != "", "Couldn't find a carry"
+    prev_carry_var=carry0_var
+    iv=input_var
+    wrong_connections=[]
+    for i in range(1,data["Nbits"]):
+        add_i_var, add_i_c = find_out(data, iv("x",i), iv("y",i), "!=")
+        zi_var, z_i_c = find_out(data, prev_carry_var, add_i_var, "!=")
+        if z_i_c == -1:
+            v1, v2, _ = find_ins(data, iv("z",i))
+            v_change = v1 if v1 != prev_carry_var else v2
+            wrong_connections.extend([v_change, add_i_var])
+            
+            switch(data, add_i_var, v_change)
+            add_i_var, add_i_c = find_out(data, iv("x",i), iv("y",i), "!=")
+            zi_var, z_i_c = find_out(data, prev_carry_var, add_i_var, "!=")
+        if zi_var != iv("z",i): # Two possible problems: either not switched to the correct zi, or the carry was wrong. 
+            wrong_connections.extend([zi_var,iv("z",i)])
+            data = switch(data, zi_var, iv("z",i)) 
+        and_i_var, and_i_c = find_out(data, iv("x",i), iv("y",i), "and")
+        carry_i_a_var, carry_i_a_c = find_out(data, add_i_var, prev_carry_var, "and")         
+        carry_i_var, carry_i_c = find_out(data, carry_i_a_var, and_i_var, "or")
+        # Now, any of those MAY BE NOT CORRECT. In that case, I need to find the switch, but how?
+        for i_c in [add_i_c, z_i_c, and_i_c, carry_i_a_c, carry_i_c]:
+            if i_c==-1:
+                print("HERE THERE IS A PROBLEM!")
+        prev_carry_var = carry_i_var
+
+    if prev_carry_var!=iv("z", i+1):#  CARRY
+        wrong_connections.append(prev_carry_var)
+        wrong_connections.append(iv("z",i+1))
+            
+    wrong_connections.sort()
+    data["wc"]=wrong_connections
+    return data
 def find_num(data):
     input_dict = data["input_dict"]
     printd(input_dict)
@@ -98,6 +194,16 @@ def solve_quiz1(fn=None, test_data=None):
     data = build_data(data)
     data=find_num(data)
     return data["num"]
+
+def solve_quiz2(fn=None, test_data=None):
+    text_data = get_data(fn, test_data)
+    data = parse(text_data)
+    data = build_data(data)
+    data = check_connection(data)
+    
+    return ",".join(data["wc"])
+
+
 
 if __name__ == "__main__":
     quiz_fn = INPUT_DIR / "day24.txt"
@@ -168,5 +274,10 @@ tnw OR pbm -> gnj"""
         
     
     solution=solve_quiz1(fn=quiz_fn)
-    check_solution(1, solution)        
+    check_solution(1, solution, 51410244478064)        
     
+
+    solution=solve_quiz2(fn=quiz_fn)
+    check_solution(2, solution, "gst,khg,nhn,tvb,vdc,z12,z21,z33")     
+
+# %%
